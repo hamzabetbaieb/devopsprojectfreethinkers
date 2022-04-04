6e3d23f312ad1090667116b52cb71623dca1c89c
mariemjedidi <meriam.jedidi@esprit.tn>
2022-04-04 16:53:01 +0100

    first commit

---
+++ src/main/java/tn/esprit/spring/entities/MissionExterne.java
@@ -0,0 +1,62 @@
+package tn.esprit.spring.entities;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class MissionExterne extends Mission {
+	
+	private static final long serialVersionUID = -3046278688391172322L;
+	
+	
+	private String emailFacturation;
+	
+	
+	private float tauxJournalierMoyen;
+
+	
+	
+
+
+	public MissionExterne(String name, String description, String emailFacturation, float tauxJournalierMoyen) {
+		super(name, description);
+		this.emailFacturation = emailFacturation;
+		this.tauxJournalierMoyen = tauxJournalierMoyen;
+	}
+
+
+	public String getEmailFacturation() {
+		return emailFacturation;
+	}
+
+
+	public void setEmailFacturation(String emailFacturation) {
+		this.emailFacturation = emailFacturation;
+	}
+
+
+	public float getTauxJournalierMoyen() {
+		return tauxJournalierMoyen;
+	}
+
+
+	public void setTauxJournalierMoyen(float tauxJournalierMoyen) {
+		this.tauxJournalierMoyen = tauxJournalierMoyen;
+	}
+	
+	
+	
+
+}
