f6048c9f3362d0aad816fceb5c395708ceb22ecc
AbirKhiarii <abir.khiari@esprit.tn>
2022-04-05 00:44:38 +0200

    commited by abir

---
+++ src/main/java/tn/esprit/spring/services/TimesheetServiceImpl.java
@@ -0,0 +1,118 @@
+package tn.esprit.spring.services;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.extern.slf4j.Slf4j;
+import tn.esprit.spring.entities.Departement;
+import tn.esprit.spring.entities.Employe;
+import tn.esprit.spring.entities.Mission;
+import tn.esprit.spring.entities.Role;
+import tn.esprit.spring.entities.Timesheet;
+import tn.esprit.spring.entities.TimesheetPK;
+import tn.esprit.spring.repository.DepartementRepository;
+import tn.esprit.spring.repository.EmployeRepository;
+import tn.esprit.spring.repository.MissionRepository;
+import tn.esprit.spring.repository.TimesheetRepository;
+
+@Slf4j
+@Service
+public class TimesheetServiceImpl implements ITimesheetService {
+	
+
+	@Autowired
+	MissionRepository missionRepository;
+	@Autowired
+	DepartementRepository deptRepoistory;
+	@Autowired
+	TimesheetRepository timesheetRepository;
+	@Autowired
+	EmployeRepository employeRepository;
+	
+	public int ajouterMission(Mission mission) {
+		log.info("Ajouter Mission commence avec succés");
+		try {
+			missionRepository.save(mission);
+			log.info("Get Id de la nouvelle mission ");
+			} catch (Exception e) 
+		{log.error("Erreur : Mission ne peut pas etre ajouter merci de verifier vos données d'entrés , plus de detail ",e );}
+		return mission.getId();
+	}
+    
+	public void affecterMissionADepartement(int missionId, int depId) {
+		log.info("Affecter Mission Departement commence avec succés");
+		Mission mission=null;
+		Departement dep = null ;
+		try {
+			log.info("Get Id de la nouvelle mission ");
+		mission = missionRepository.findById(missionId).get();
+		dep = deptRepoistory.findById(depId).get();
+		mission.setDepartement(dep);
+		missionRepository.save(mission);}
+		catch (Exception e) 
+		{log.error("Erreur : Mission ne peut pas etre affecté au departement merci de verifier vos données d'entrés , plus de detail ",e );}		
+	}
+
+	public void ajouterTimesheet(int missionId, int employeId, Date dateDebut, Date dateFin) {
+		TimesheetPK timesheetPK = new TimesheetPK();
+		timesheetPK.setDateDebut(dateDebut);
+		timesheetPK.setDateFin(dateFin);
+		timesheetPK.setIdEmploye(employeId);
+		timesheetPK.setIdMission(missionId);
+		
+		Timesheet timesheet = new Timesheet();
+		timesheet.setTimesheetPK(timesheetPK);
+		timesheet.setValide(false); //par defaut non valide
+		timesheetRepository.save(timesheet);
+		
+	}
+
+	
+	public void validerTimesheet(int missionId, int employeId, Date dateDebut, Date dateFin, int validateurId) {
+		System.out.println("In valider Timesheet");
+		Employe validateur = employeRepository.findById(validateurId).get();
+		Mission mission = missionRepository.findById(missionId).get();
+		//verifier s'il est un chef de departement (interet des enum)
+		if(!validateur.getRole().equals(Role.CHEF_DEPARTEMENT)){
+			System.out.println("l'employe doit etre chef de departement pour valider une feuille de temps !");
+			return;
+		}
+		//verifier s'il est le chef de departement de la mission en question
+		boolean chefDeLaMission = false;
+		for(Departement dep : validateur.getDepartements()){
+			if(dep.getId() == mission.getDepartement().getId()){
+				chefDeLaMission = true;
+				break;
+			}
+		}
+		if(!chefDeLaMission){
+			System.out.println("l'employe doit etre chef de departement de la mission en question");
+			return;
+		}
+//
+		TimesheetPK timesheetPK = new TimesheetPK(missionId, employeId, dateDebut, dateFin);
+		Timesheet timesheet =timesheetRepository.findBytimesheetPK(timesheetPK);
+		timesheet.setValide(true);
+		
+		//Comment Lire une date de la base de données
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		System.out.println("dateDebut : " + dateFormat.format(timesheet.getTimesheetPK().getDateDebut()));
+		
+	}
+
+	
+	public List<Mission> findAllMissionByEmployeJPQL(int employeId) {
+		return timesheetRepository.findAllMissionByEmployeJPQL(employeId);
+	}
+
+	
+	public List<Employe> getAllEmployeByMission(int missionId) {
+		return timesheetRepository.getAllEmployeByMission(missionId);
+	}
+
+}
